29a1bfb1006cff1a30492d8bae0889fe46460959
RyotaTomiyama <lit.karu.noir@gmail.com>
2018-06-03 02:10:06 +0900

    finish kadai

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -2,9 +2,15 @@ package codecheck;
 
 public class App {
 	public static void main(String[] args) {
-		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
-			System.out.println(output);
-		}
+		// for (int i = 0, l = args.length; i < l; i++) {
+		// 	String output = String.format("argv[%s]: %s", i, args[i]);
+		// 	System.out.println(output);
+		// }
+
+		if(args[i].equals("World")) System.out.println("Hello World!");
+		if(args[i].equals("織田信長")) System.out.println("Hello 織田信長!");
+		if(args[i].equals("codecheck")) System.out.println("Hello codecheck!");
+
 	}
+	
 }
